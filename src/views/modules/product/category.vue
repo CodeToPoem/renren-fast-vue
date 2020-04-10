19deb461d1f2920ffa080011c0b91ef6d1da930f
joey <763150048@qq.com>
2020-04-10 21:55:50 +0800

    环境搭建&三级菜单

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(({ data }) => {
+        console.log("成功获取到菜单", data.data);
+        this.menus = data.data;
+      });
+    }
+  },
+  computed: {},
+  created() {
+    this.getMenus();
+  },
+  mounted() {},
+  watch: {},
+  components: {}
+};
+</script>
+
+<style scoped>
+</style>
