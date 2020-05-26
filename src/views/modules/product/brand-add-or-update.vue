62b11107c41362ee8c8a4961914b8e9a1c57aec9
joey <763150048@qq.com>
2020-05-26 16:39:44 +0800

    产品信息-前端数据校验

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -16,21 +16,25 @@
       </el-form-item>
       <el-form-item label="品牌logo地址" prop="logo">
         <!-- <el-input v-model="dataForm.logo" placeholder="品牌logo地址"></el-input> -->
-        <singleUpload v-model="dataForm.logo">
-
-        </singleUpload>
+        <singleUpload v-model="dataForm.logo"></singleUpload>
       </el-form-item>
       <el-form-item label="介绍" prop="descript">
         <el-input v-model="dataForm.descript" placeholder="介绍"></el-input>
       </el-form-item>
       <el-form-item label="显示状态" prop="showStatus">
-        <el-switch v-model="dataForm.showStatus" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        <el-switch
+          v-model="dataForm.showStatus"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+          :active-value="1"
+          :inactive-value="0"
+        ></el-switch>
       </el-form-item>
       <el-form-item label="检索首字母" prop="firstLetter">
         <el-input v-model="dataForm.firstLetter" placeholder="检索首字母"></el-input>
       </el-form-item>
       <el-form-item label="排序" prop="sort">
-        <el-input v-model="dataForm.sort" placeholder="排序"></el-input>
+        <el-input v-model.number="dataForm.sort" placeholder="排序"></el-input>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -41,9 +45,9 @@
 </template>
 
 <script>
-import singleUpload from "@/components/upload/singleUpload"
+import singleUpload from "@/components/upload/singleUpload";
 export default {
-  components:{singleUpload},
+  components: { singleUpload },
   data() {
     return {
       visible: false,
@@ -52,9 +56,9 @@ export default {
         name: "",
         logo: "",
         descript: "",
-        showStatus: "",
+        showStatus: 1,
         firstLetter: "",
-        sort: ""
+        sort: 0
       },
       dataRule: {
         name: [{ required: true, message: "品牌名不能为空", trigger: "blur" }],
@@ -72,9 +76,33 @@ export default {
           }
         ],
         firstLetter: [
-          { required: true, message: "检索首字母不能为空", trigger: "blur" }
+          {
+            validator: (rule, value, callback) => {
+              if (value === "") {
+                callback(new Error("首字母必须填写"));
+              } else if (!/^[A-Za-z]$/.test(value)) {
+                callback(new Error("首字母必须必须a-z或者A-Z"));
+              } else {
+                callback();
+              }
+            },
+            trigger: "blur"
+          }
         ],
-        sort: [{ required: true, message: "排序不能为空", trigger: "blur" }]
+        sort: [
+          {
+            validator: (rule, value, callback) => {
+              if (value === "") {
+                callback(new Error("排序字段必须填写"));
+              } else if (!Number.isInteger(value)||value<0) {
+                callback(new Error("首字母必须是一个大于等于0的整数"));
+              } else {
+                callback();
+              }
+            },
+            trigger: "blur"
+          }
+        ]
       }
     };
   },
